c28a522cb068333919e5843a28de8b144116c689
zzk <zzk@github.com>
2026-04-21 19:40:19 +0800

    Fix test case and update VSCode tasks

--- src/solution.cpp
+++ src/solution.cpp
@@ -1,3 +1,21 @@
 #include "solution.h"
 
-bool Solution::isSymmetric(TreeNode* root) {}
+bool isSymmetricTwo(TreeNode* left, TreeNode* right) {
+    if (left == right) {
+        return true;
+    }
+    if (left == NULL || right == NULL) {
+        return false;
+    }
+    if (left->val != right->val) {
+        return false;
+    }
+    return isSymmetricTwo(left->left, right->right) &&
+           isSymmetricTwo(left->right, right->left);
+}
+bool Solution::isSymmetric(TreeNode* root) {
+    if (root == NULL) {
+        return true;
+    }
+    return isSymmetricTwo(root->left, root->right);
+}
